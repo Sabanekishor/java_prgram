47a261e27af94b9f01d5c5cdd6b5f1228242f347
ADMIN <ADMIN@DESKTOP-US0QBG5>
2023-11-30 18:46:49 +0530

    OOPS_Assingnment

---
+++ src/java_prgram/ObjectClass.java
@@ -0,0 +1,132 @@
+package java_prgram;
+
+public class ObjectClass {
+	public static void main(String[] args) {
+		
+		class ATM_Card {
+			int cardNumber;
+			String Material;
+			int CVV;
+			void Transactions() {
+				
+			}
+			
+
+		}
+	class	Bag {
+			String size;
+			int weight;
+			String colour;
+			String pockets;
+		void carrying() {
+			
+		}
+
+		}
+		class Bike {
+			String Brand;
+			String color;
+			int Speed;
+			int Average;
+			void Riding() {
+				
+			}
+			}
+	class	Book {
+			
+			String Name;
+			String Subject;
+			int Pages;
+			 void Reading() {
+				
+			}
+			
+		}
+
+	class	CricketBat {
+			String material;
+			double weight;
+			int height;
+			void playing() {
+				
+			}
+
+		}
+
+		class DoorBell {
+			String Brand;
+			String Button;
+			int size;
+			void Ringing() {
+				
+			}
+
+		}
+
+		class Fan {
+			String Brand;
+			String color;
+			int Blades;
+			void cooling() {
+				
+			}
+		}
+
+		class Headphone {
+
+			String Brand;
+			String Type;
+			double Range;
+			int Battery;
+			void Listening() {
+				
+			}
+
+		}
+
+		class Mobile {
+			String Brand;
+			String color;
+			int size;
+			void calling() {
+				
+			}
+			
+
+		}
+		class School {
+			String Medium;
+			int Standard;
+			int Teachers;
+			int Student;
+			void Learning() {
+				
+			}
+			
+		}
+
+		class Telivision {
+			String Brand;
+			String color;
+			double size;
+			void ing() {
+				
+			}
+
+		}
+
+		class Tyre {
+
+			String Brand;
+			String shape;
+			int size;
+			void rolling() {
+				
+			}
+			
+
+		}
+
+	}
+
+}
